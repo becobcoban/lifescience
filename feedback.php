713617651427d9585e3f4887a68262d37b3a5fc0
Bryan Emmanuel Cobcoban <becobcoban@snapworxdigital.com>
2016-05-27 17:59:06 +0800

    update on feedback page

--- feedback.php
+++ feedback.php
@@ -1,83 +1,65 @@
 <?php include_once 'header.php'; ?>
 
-<main class="faqs">
+<main class="feedback">
 	<div class="container">
-		<div class="faqs-content">
-			<div class="row">
-				<div class="col-md-12">
-					<h1 class="heading faqs-heading">FAQS</h1>
-					<div class="accordion">
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">How long has LifeScience been around?</div>
-							<div class="accordion-panel-content">
-								<p>LifeScience Center for Health and Wellness has been open since 2010.  We have been providing customized healthcare services for local and international clients since then.</p>
-							</div>
+			<h1 class="heading feedback-heading">FEEDBACKS</h1>
+			<p>Thank you for participating in our client satisfaction survey. <br>
+			We would love to hear about your LifeScience experience, your answers below will help us improve our services.</p>
+			<form class="form form--feedback" id="form-feedback" action="">
+				<hr class="feedback-separator">
+
+				<div class="row feedback-group">
+					<div class="col-md-12 form-group">
+						<label class="feedback-label" for="first_name">First Name</label>
+						<input type="text" class="form-control feedback-input-sm" id="first_name" name="first_name">
+						<label class="feedback-label-sm" for="last_name">Last Name</label>
+						<input type="text" class="form-control feedback-input-sm" id="last_name" name="last_name">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="feedback-label" for="address">Address</label>
+						<input type="text" class="form-control feedback-input" style="display: inline-block;" id="address" name="address">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="feedback-label" for="email">E-mail</label>
+						<input type="text" class="form-control feedback-input" id="email" name="email">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="feedback-label" for="regular_client">Client</label>
+						<div class="feedback-checkbox">
+							<input type="checkbox" class="form-checkbox single-select" id="regular_client" name="client" value="regular">
+							<label class="feedback-checkbox-label" for="regular_client">Regular Client</label>
 						</div>
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">I have a family history of certain diseases, how can LifeScience help?</div>
-							<div class="accordion-panel-content">
-								<p>We take the guesswork out of healthcare! Eliminate the "what ifs" and find out for certain. With a gene test, we can tell you which diseases you should and should not worry about.</p>
-								<p>Plus, the results of your gene test gives us a tremendous amount of information about your body and helps us design a custom program to address your health issues effectively and help you achieve optimum health.</p>
-							</div>
-						</div>
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">Is hormone therapy safe?</div>
-							<div class="accordion-panel-content">
-								<p>We at LifeScience have your health and well-being as our number one priority. We do not offer any products or services that are not certified internationally.</p>
-								<p>Hormone therapy is an effective way to address hormone imbalances, but it is not for everyone. Our medical experts will first determine whether hormone replacement therapy is right for you before moving forward with any procedures.</p>
-							</div>
-						</div>
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">What is micronutrient deficiency testing?</div>
-							<div class="accordion-panel-content">
-								<p>Micronutrient deficiencies occur when the body lacks any essential nutrients like vitamins or minerals. Fatigue, weakness, hair loss, and even depression are just some of the symptoms of micronutrient deficiency. If left untreated, this can lead to serious diseases.</p>
-								<p>A micronutrient profile test will easily let us know what nutrients your body is missing and allows us to address deficiencies with customized supplements designed especially for you. Call 828-LIFE(5433) to learn more about our services and signature programs.</p>
-							</div>
-						</div>
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">How do I prepare for my appointment?</div>
-							<div class="accordion-panel-content">
-								<p>The more we know about you, the better! We always begin with a comprehensive assessment of your health history, your current lifestyle and state of wellness.  You will have to fill up a form that will detail all of the information about you that will be necessary in giving your doctor an accurate health profile.  From there, your conversation and your journey with us begins.</p>
-							</div>
-						</div>
-						<div class="accordion-panel">
-							<div class="accordion-panel-link">Do you process insurance claims?</div>
-							<div class="accordion-panel-content">
-								<p>We do not process insurance claims at the moment.</p>
-							</div>
+						<div class="feedback-checkbox">
+							<input type="checkbox" class="form-checkbox single-select" id="firsttime_client" name="client" value="first time">
+							<label class="feedback-checkbox-label" for="firsttime_client">First Time Client</label>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div><!-- /.faqs-content -->
-		<div class="faqs-inquiries">
-			<div class="row">
-				<div class="col-md-4">
-					<h1 class="heading faqs-heading">FOR INQUIRIES</h1>
-					<p>If your concern is not listed here, email us your query, and we’ll get back to you.</p>
+
+				<hr class="feedback-separator">
+
+				<div class="row feedback-group">
+					<div class="col-md-12 form-group">
+						<label class="form-label" for="last_visit">When did you last visit our clinic?</label>
+						<input type="text" class="form-control" id="last_visit" name="last_visit">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="form-label" for="experience">How has your experience been so far across all your visits?</label>
+						<input type="text" class="form-control" id="experience" name="experience">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="form-label" for="improve">In what ways can we improve your experience with us?</label>
+						<input type="text" class="form-control" id="improve" name="improve">
+					</div>
+					<div class="col-md-12 form-group">
+						<label class="form-label" for="message">Please share with us the name of the doctor/staff that you would like to commend and a message you would like to give them.</label>
+						<input type="text" class="form-control" id="message" name="message">
+					</div>
 				</div>
-				<div class="col-md-8">
-					<form class="form form--inquiries" action="">
-						<div class="col-md-6 form-group">
-							<label class="form-label" for="first_name">First Name</label>
-							<input type="text" class="form-control" id="first_name" name="first_name">
-						</div>
-						<div class="col-md-6 form-group">
-							<label class="form-label" for="last_name">Last Name</label>
-							<input type="text" class="form-control" id="last_name" name="last_name">
-						</div>
-						<div class="col-md-12 form-group">
-							<label class="form-label" for="email">E-mail</label>
-							<input type="text" class="form-control" id="email" name="email">
-						</div>
-						<div class="col-md-12 form-group">
-							<label class="form-label" for="message">Message</label>
-							<textarea class="form-control" name="message" id="message" cols="30" rows="8"></textarea>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div><!-- /.faqs-inquiries -->
+
+				<hr class="feedback-separator">
+			</form>
+		</div>
 	</div>
 </main>
 
